8d55aac9feb03568b9c266d65ba58c1013843103
zmtq05 <zmtq05@gmail.com>
2021-12-03 20:16:09 +0900

    Day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -7,7 +7,7 @@ pub fn generator(input: &str) -> Vec<Vec<u32>> {
 
 pub fn part1(lines: &[Vec<u32>]) -> u32 {
     let modes: Vec<u32> = (0..12)
-        .map(|i| get_mode(lines, i, |zero, one| zero > one))
+        .map(|i| get_mode(lines, i))
         .collect();
     let gamma = cal_binary_sum(&modes);
     let epsilon = cal_binary_sum(&reverse_bit(&modes));
@@ -19,7 +19,7 @@ pub fn part2(lines: &[Vec<u32>]) -> u32 {
     let oxygen = {
         let mut cloned = lines.to_vec();
         for i in 0..12 {
-            let mode = get_mode(&cloned, i, |zero, one| zero > one);
+            let mode = get_mode(&cloned, i);
             cloned.retain(|line| line[i] == mode);
             if cloned.len() == 1 {
                 break;
@@ -29,26 +29,24 @@ pub fn part2(lines: &[Vec<u32>]) -> u32 {
     };
 
     let co2 = {
-        let mut reversed: Vec<Vec<u32>> = lines.iter().map(|line| reverse_bit(line)).collect();
+        let mut cloned = lines.to_vec();
         for i in 0..12 {
-            let mode = get_mode(&reversed, i, |zero, one| zero >= one);
-            reversed.retain(|line| line[i] == mode);
-            if reversed.len() == 1 {
+            let mode = get_mode(&cloned, i) ^ 1;
+            cloned.retain(|line| line[i] == mode);
+            if cloned.len() == 1 {
                 break;
             }
         }
-        cal_binary_sum(reversed[0].as_ref())
+        cal_binary_sum(cloned[0].as_ref())
     };
     oxygen * co2
 }
 
-fn get_mode<F>(lines: &[Vec<u32>], i: usize, f: F) -> u32
-where
-    F: Fn(usize, usize) -> bool,
+fn get_mode(lines: &[Vec<u32>], i: usize) -> u32
 {
     let zero = lines.iter().filter(|line| line[i] == 0).count();
     let one = lines.len() - zero;
-    if f(zero, one) {
+    if zero > one {
         0
     } else {
         1
