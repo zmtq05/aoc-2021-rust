a7feefea354a2009da930b2b3a100e3721b29199
zmtq05 <zmtq05@gmail.com>
2021-12-03 18:44:48 +0900

    Optimize Day 3 part 1

--- src/day3.rs
+++ src/day3.rs
@@ -1,36 +1,39 @@
-pub fn generator(input: &str) -> Vec<&str> {
-    input.lines().collect()
+pub fn generator(input: &str) -> Vec<Vec<u32>> {
+    input
+        .lines()
+        .map(|line| line.chars().map(|ch| ch.to_digit(2).unwrap()).collect())
+        .collect()
 }
 
-pub fn part1(input: &[&str]) -> u32 {
-    let mut store = [0; 12];
-    for line in input {
-        for (i, b) in line.chars().enumerate() {
-            store[i] += match b {
-                '1' => 1,
-                '0' => 0,
-                _ => unreachable!(),
-            }
-        }
-    }
-    let mode: Vec<_> = store
-        .iter()
-        .map(|&b| if b as usize > input.len() / 2 { 1 } else { 0 })
+pub fn part1(lines: &[Vec<u32>]) -> u32 {
+    let modes: Vec<u32> = (0..12)
+        .map(|i| get_mode(lines, i, |zero, one| zero > one))
         .collect();
-
-    let gamma = {
-        let gamma = mode.iter().map(|b| b.to_string()).collect::<String>();
-        u32::from_str_radix(&gamma, 2).unwrap()
-    };
-
-    let epsilon = {
-        let e: String = mode
-            .iter()
-            .map(|&b| if b == 1 { 0 } else { 1 })
-            .map(|b| b.to_string())
-            .collect();
-        u32::from_str_radix(&e, 2).unwrap()
-    };
+    let gamma = cal_binary_sum(&modes);
+    let epsilon = cal_binary_sum(&reverse_bit(&modes));
 
     gamma * epsilon
+}
+
+fn get_mode<F>(lines: &[Vec<u32>], i: usize, f: F) -> u32
+where
+    F: Fn(usize, usize) -> bool,
+{
+    let zero = lines.iter().filter(|line| line[i] == 0).count();
+    let one = lines.len() - zero;
+    if f(zero, one) {
+        0
+    } else {
+        1
+    }
+}
+
+fn cal_binary_sum(arr: &[u32]) -> u32 {
+    arr.iter()
+        .enumerate()
+        .fold(0, |acc, (i, v)| acc + (v << (11 - i)))
+}
+
+fn reverse_bit(arr: &[u32]) -> Vec<u32> {
+    arr.iter().map(|b| b ^ 1).collect()
 }
