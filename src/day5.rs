7c5b45b75eaec6333896e2dcd77ffb058f8c1e33
zmtq05 <zmtq05@gmail.com>
2021-12-05 20:03:27 +0900

    Refactor: Day5-1

--- src/day5.rs
+++ src/day5.rs
@@ -1,54 +1,67 @@
+use std::{collections::HashMap, ops::RangeInclusive, str::FromStr};
+
 #[derive(Clone, Debug)]
 pub struct Vent {
-    start: Position,
-    end: Position,
+    x1: i32,
+    x2: i32,
+    y1: i32,
+    y2: i32,
 }
 
-#[derive(Clone, Debug)]
-struct Position {
-    x: i32,
-    y: i32,
-}
+impl FromStr for Vent {
+    type Err = ();
 
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let (xy1, xy2) = line.split_once(" -> ").unwrap();
+        let (x1, y1) = xy1
+            .split_once(',')
+            .map(|xy| (xy.0.parse().unwrap(), xy.1.parse().unwrap()))
+            .unwrap();
+        let (x2, y2) = xy2
+            .split_once(',')
+            .map(|xy| (xy.0.parse().unwrap(), xy.1.parse().unwrap()))
+            .unwrap();
+        Ok(Self { x1, x2, y1, y2 })
+    }
+}
 pub fn generator(input: &str) -> Vec<Vent> {
-    input
-        .lines()
-        .map(|line| line.split_once("->").unwrap())
-        .map(|(start, end)| {
-            let parser =
-                |(x, y): (&str, &str)| (x.trim().parse().unwrap(), y.trim().parse().unwrap());
-            let start = start
-                .split_once(',')
-                .map(parser)
-                .map(|(x, y)| Position { x, y })
-                .unwrap();
-            let end = end
-                .split_once(',')
-                .map(parser)
-                .map(|(x, y)| Position { x, y })
-                .unwrap();
-            Vent { start, end }
-        })
-        .collect()
+    input.lines().map(|line| line.parse().unwrap()).collect()
+}
+
+enum Slope {
+    Horizontal(i32, RangeInclusive<i32>),
+    Vertical(i32, RangeInclusive<i32>),
+    Diagonal(RangeInclusive<i32>, RangeInclusive<i32>),
+}
+
+impl From<&Vent> for Slope {
+    fn from(vent: &Vent) -> Self {
+        let Vent { x1, x2, y1, y2 } = *vent;
+        let x_range = if x1 > x2 { x2..=x1 } else { x1..=x2 };
+        let y_range = if y1 > y2 { y2..=y1 } else { y1..=y2 };
+        if x1 == x2 {
+            Slope::Vertical(x1, y_range)
+        } else if y1 == y2 {
+            Slope::Horizontal(y1, x_range)
+        } else {
+            Slope::Diagonal(x_range, y_range)
+        }
+    }
 }
 
 pub fn part1(vents: &[Vent]) -> usize {
-    let mut map = [[0; 1000]; 1000];
-    vents
-        .iter()
-        .filter(|v| v.start.x == v.end.x || v.start.y == v.end.y)
-        .for_each(|vent| {
-            let Vent { start, end } = vent;
-            let min = start.y.min(end.y);
-            let max = start.y.max(end.y);
-            for y in min..max + 1 {
-                let min = start.x.min(end.x);
-                let max = start.x.max(end.x);
-                for x in min..max + 1 {
-                    map[y as usize][x as usize] += 1;
-                }
-            }
-        });
+    let mut map = HashMap::new();
+    vents.iter().for_each(|vent| {
+        match Slope::from(vent) {
+            Slope::Horizontal(x, y_range) => y_range
+                .into_iter()
+                .for_each(|y| *map.entry((x, y)).or_insert(0) += 1),
+            Slope::Vertical(y, x_range) => x_range
+                .into_iter()
+                .for_each(|x| *map.entry((x, y)).or_insert(0) += 1),
+            Slope::Diagonal(_, _) => {}
+        };
+    });
 
-    map.iter().flatten().filter(|x| **x > 1).count()
+    map.values().filter(|x| **x > 1).count()
 }
