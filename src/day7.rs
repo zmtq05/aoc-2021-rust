ef3ca02b0a8e211f915f27e9b1e261ff202d293c
zmtq05 <zmtq05@gmail.com>
2021-12-07 18:26:28 +0900

    Refactor: Day7

--- src/day7.rs
+++ src/day7.rs
@@ -1,8 +1,10 @@
-pub fn generator(input: &str) -> Vec<u32> {
+use std::ops::Sub;
+
+pub fn generator(input: &str) -> Vec<i32> {
     input.split(',').map(|a| a.parse().unwrap()).collect()
 }
 
-pub fn part1(crabs: &[u32]) -> u32 {
+pub fn part1(crabs: &[i32]) -> i32 {
     let median = {
         let mut v = crabs.to_vec();
         v.sort_unstable();
@@ -10,28 +12,17 @@ pub fn part1(crabs: &[u32]) -> u32 {
         v[mid]
     };
 
-    crabs
-        .iter()
-        .map(|&crab| {
-            if crab > median {
-                crab - median
-            } else {
-                median - crab
-            }
-        })
-        .sum()
+    crabs.iter().map(|pos| median.sub(pos).abs()).sum()
 }
 
-pub fn part2(crabs: &[u32]) -> u32 {
+pub fn part2(crabs: &[i32]) -> i32 {
     let max = *crabs.iter().max().unwrap();
     (0..=max)
         .map(|i| {
             crabs
                 .iter()
-                .map(|&pos| {
-                    let diff = if pos > i { pos - i } else { i - pos };
-                    diff * (diff + 1) / 2
-                })
+                .map(|pos| i.sub(pos).abs())
+                .map(|diff| diff * (diff + 1) / 2)
                 .sum()
         })
         .min()
