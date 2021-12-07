19fac0cbb5fefb868260dc43617688edd8fe9bed
zmtq05 <zmtq05@gmail.com>
2021-12-07 18:22:55 +0900

    Opt: Day7-2

--- src/day7.rs
+++ src/day7.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 pub fn generator(input: &str) -> Vec<u32> {
     input.split(',').map(|a| a.parse().unwrap()).collect()
 }
@@ -26,16 +24,16 @@ pub fn part1(crabs: &[u32]) -> u32 {
 
 pub fn part2(crabs: &[u32]) -> u32 {
     let max = *crabs.iter().max().unwrap();
-
-    let mut fuels = HashMap::new();
-
-    (0..=max).for_each(|x| {
-        crabs.iter().for_each(|&crab| {
-            let fuel = fuels.entry(x).or_default();
-            let diff = if crab > x { crab - x } else { x - crab };
-            *fuel += diff * (diff + 1) / 2;
+    (0..=max)
+        .map(|i| {
+            crabs
+                .iter()
+                .map(|&pos| {
+                    let diff = if pos > i { pos - i } else { i - pos };
+                    diff * (diff + 1) / 2
+                })
+                .sum()
         })
-    });
-
-    *fuels.values().min().unwrap()
+        .min()
+        .unwrap()
 }
