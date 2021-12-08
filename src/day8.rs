e6757f5c99c76a2240c49d3f96882e8b4f42ad59
zmtq05 <zmtq05@gmail.com>
2021-12-08 23:49:24 +0900

    Solve: Day8-2

--- src/day8.rs
+++ src/day8.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 pub fn generator(input: &str) -> (Vec<Vec<&str>>, Vec<Vec<&str>>) {
     let split_iter = input.lines().map(|l| l.split_once(" | ").unwrap());
 
@@ -20,4 +22,65 @@ pub fn part1((_, outputs): &(Vec<Vec<&str>>, Vec<Vec<&str>>)) -> usize {
             len == 2 || len == 3 || len == 4 || len == 7
         })
         .count()
+}
+
+pub fn part2((wire_lines, output_lines): &(Vec<Vec<&str>>, Vec<Vec<&str>>)) -> usize {
+    let mut sum = 0;
+    for (wires, outputs) in wire_lines.iter().zip(output_lines) {
+        let decoder = Decoder::new(wires);
+        for (i, output) in outputs.iter().rev().enumerate() {
+            // let output = outputs[i];
+            let segment = decoder.decode(output);
+            sum += segment * 10usize.pow(i as u32);
+        }
+    }
+    sum
+}
+
+struct Decoder {
+    one: HashSet<char>,
+    four: HashSet<char>,
+}
+
+impl Decoder {
+    fn new(wires: &[&str]) -> Self {
+        fn rename_me(wires: &[&str], len: usize) -> HashSet<char> {
+            wires
+                .iter()
+                .filter(|wire| wire.len() == len)
+                .take(1)
+                .next()
+                .unwrap()
+                .chars()
+                .collect()
+        }
+        let one = rename_me(wires, 2);
+        let four = rename_me(wires, 4);
+
+        Self { one, four }
+    }
+
+    fn int1count(&self, other: &str) -> usize {
+        self.one.intersection(&other.chars().collect()).count()
+    }
+
+    fn int4count(&self, other: &str) -> usize {
+        self.four.intersection(&other.chars().collect()).count()
+    }
+
+    fn decode(&self, other: &str) -> usize {
+        match other.len() {
+            2 => 1,
+            3 => 7,
+            4 => 4,
+            5 if self.int1count(other) == 2 => 3,
+            5 if self.int4count(other) == 3 => 5,
+            5 => 2,
+            6 if self.int1count(other) == 1 => 6,
+            6 if self.int4count(other) == 4 => 9,
+            6 => 0,
+            7 => 8,
+            _ => unreachable!(),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ aoc_main::main! {
     day5 : generator => part1, part2;
     day6 : generator => part1, part2;
     day7 : generator => part1, part2;
-    day8 : generator => part1;
+    day8 : generator => part1, part2;
 }
 
 mod day1;
