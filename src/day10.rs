e332b0a696b178c446ff4f154d0c358e053e2c47
zmtq05 <zmtq05@gmail.com>
2021-12-10 22:11:16 +0900

    Refactor: Day10-1

--- src/day10.rs
+++ src/day10.rs
@@ -1,125 +1,148 @@
-use std::collections::VecDeque;
-
-pub fn generator(input: &str) -> Vec<VecDeque<char>> {
-    input.lines().map(|line| line.chars().collect()).collect()
+pub fn generator(input: &str) -> Vec<Vec<Bracket>> {
+    input
+        .lines()
+        .map(|line| line.chars().map(Bracket::from).collect())
+        .collect()
 }
 
-pub fn part1(lines: &[VecDeque<char>]) -> u32 {
+pub fn part1(lines: &[Vec<Bracket>]) -> u32 {
     let mut sum = 0;
-    for mut line in lines.to_vec() {
-        let mut states = vec![];
-        while let Some(bracket) = line.pop_front() {
-            let state = State::from(bracket);
-            match &state {
-                State::Open(_) => states.push(state),
-                State::Close(bracket) => if let Some(point) = pop_or_point(&mut states, bracket) {
-                    sum += point;
+
+    for line in lines {
+        let mut open_brackets: Vec<Kind> = vec![];
+
+        for bracket in line {
+            match bracket.state {
+                State::Open => open_brackets.push(bracket.kind.clone()),
+                State::Close => if open_brackets.pop().unwrap() != bracket.kind {
+                    sum += bracket.kind.point();
                     break;
-                },
-            };
+                }
+            }
         }
     }
 
     sum
 }
 
-pub fn part2(lines: &[VecDeque<char>]) -> u64 {
-    let mut scores = vec![];
-    'outer: for mut line in lines.to_vec() {
-        let mut sum = 0;
-        let mut states = vec![];
-        while let Some(bracket) = line.pop_front() {
-            let state = State::from(bracket);
-            match &state {
-                State::Open(_) => states.push(state),
-                State::Close(bracket) => {
-                    if let Some(last) = states.last() {
-                        match last {
-                            State::Open(last_bracket) if last_bracket == bracket => {
-                                states.pop();
-                            }
-                            _ => continue 'outer,
-                        }
-                    } else {
-                        continue 'outer;
-                    }
-                }
-            }
-        }
+// pub fn part2(lines: &[VecDeque<char>]) -> u64 {
+//     let mut scores = vec![];
+//     'outer: for mut line in lines.to_vec() {
+//         let mut sum = 0;
+//         let mut states = vec![];
+//         while let Some(bracket) = line.pop_front() {
+//             let state = State::from(bracket);
+//             match &state {
+//                 State::Open(_) => states.push(state),
+//                 State::Close(bracket) => {
+//                     if let Some(last) = states.last() {
+//                         match last {
+//                             State::Open(last_bracket) if last_bracket == bracket => {
+//                                 states.pop();
+//                             }
+//                             _ => continue 'outer,
+//                         }
+//                     } else {
+//                         continue 'outer;
+//                     }
+//                 }
+//             }
+//         }
 
-        for state in states.into_iter().rev() {
-            match state {
-                State::Open(bracket) => {
-                    sum *= 5;
-                    sum += bracket.point2();
-                }
-                State::Close(_) => unreachable!(),
-            }
-        }
+//         for state in states.into_iter().rev() {
+//             match state {
+//                 State::Open(bracket) => {
+//                     sum *= 5;
+//                     sum += bracket.point2();
+//                 }
+//                 State::Close(_) => unreachable!(),
+//             }
+//         }
 
-        scores.push(sum);
-    }
-    scores.sort_unstable();
-    scores[scores.len() / 2]
+//         scores.push(sum);
+//     }
+//     scores.sort_unstable();
+//     scores[scores.len() / 2]
+// }
+
+#[derive(Clone)]
+pub struct Bracket {
+    kind: Kind,
+    state: State,
 }
 
-fn pop_or_point(states: &mut Vec<State>, bracket: &Bracket) -> Option<u32> {
-    if let Some(State::Open(last_bracket)) = states.last() {
-        if last_bracket == bracket {
-            states.pop();
-            return None;
+impl From<char> for Bracket {
+    fn from(char: char) -> Self {
+        use Kind::*;
+        use State::*;
+        match char {
+            '(' => Self {
+                kind: Parentheses,
+                state: Open,
+            },
+            ')' => Self {
+                kind: Parentheses,
+                state: Close,
+            },
+            '{' => Self {
+                kind: Curly,
+                state: Open,
+            },
+            '}' => Self {
+                kind: Curly,
+                state: Close,
+            },
+            '[' => Self {
+                kind: Square,
+                state: Open,
+            },
+            ']' => Self {
+                kind: Square,
+                state: Close,
+            },
+            '<' => Self {
+                kind: Angle,
+                state: Open,
+            },
+            '>' => Self {
+                kind: Angle,
+                state: Close,
+            },
+            _ => unreachable!(),
         }
     }
-    Some(bracket.point())
 }
 
-#[derive(PartialEq, Eq)]
-enum Bracket {
+#[derive(PartialEq, Eq, Clone)]
+pub enum Kind {
     Parentheses,
     Square,
     Curly,
     Angle,
 }
 
-impl Bracket {
+impl Kind {
     fn point(&self) -> u32 {
         match self {
-            Bracket::Parentheses => 3,
-            Bracket::Square => 57,
-            Bracket::Curly => 1197,
-            Bracket::Angle => 25137,
+            Kind::Parentheses => 3,
+            Kind::Square => 57,
+            Kind::Curly => 1197,
+            Kind::Angle => 25137,
         }
     }
 
     fn point2(&self) -> u64 {
         match self {
-            Bracket::Parentheses => 1,
-            Bracket::Square => 2,
-            Bracket::Curly => 3,
-            Bracket::Angle => 4,
+            Kind::Parentheses => 1,
+            Kind::Square => 2,
+            Kind::Curly => 3,
+            Kind::Angle => 4,
         }
     }
 }
 
-impl From<char> for State {
-    fn from(char: char) -> Self {
-        use Bracket::*;
-        use State::*;
-        match char {
-            '(' => Open(Parentheses),
-            ')' => Close(Parentheses),
-            '{' => Open(Curly),
-            '}' => Close(Curly),
-            '[' => Open(Square),
-            ']' => Close(Square),
-            '<' => Open(Angle),
-            '>' => Close(Angle),
-            _ => unreachable!(),
-        }
-    }
-}
-
-enum State {
-    Open(Bracket),
-    Close(Bracket),
+#[derive(Clone)]
+pub enum State {
+    Open,
+    Close,
 }
