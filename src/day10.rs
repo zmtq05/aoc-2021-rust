9752cb4470d14cdaaf6d61581835bbeaae90cf21
zmtq05 <zmtq05@gmail.com>
2021-12-10 21:01:25 +0900

    Solve: Day10-2

--- src/day10.rs
+++ src/day10.rs
@@ -22,6 +22,46 @@ pub fn part1(lines: &[VecDeque<char>]) -> u32 {
     sum
 }
 
+pub fn part2(lines: &[VecDeque<char>]) -> u64 {
+    let mut scores = vec![];
+    'outer: for mut line in lines.to_vec() {
+        let mut sum = 0;
+        let mut states = vec![];
+        while let Some(bracket) = line.pop_front() {
+            let state = State::from(bracket);
+            match &state {
+                State::Open(_) => states.push(state),
+                State::Close(bracket) => {
+                    if let Some(last) = states.last() {
+                        match last {
+                            State::Open(last_bracket) if last_bracket == bracket => {
+                                states.pop();
+                            },
+                            _ => continue 'outer,
+                        }
+                    } else {
+                        continue 'outer;
+                    }
+                },
+            }
+        }
+
+        for state in states.into_iter().rev() {
+            match state {
+                State::Open(bracket) => {
+                    sum *= 5;
+                    sum += bracket.point2();
+                }
+                State::Close(_) => unreachable!(),
+            }
+        }
+
+        scores.push(sum);
+    }
+    scores.sort_unstable();
+    scores[scores.len() / 2]
+}
+
 fn rename_me(states: &mut Vec<State>, bracket: &Bracket, sum: &mut u32) -> bool {
     if let Some(last) = states.last() {
         if let State::Open(last_bracket) = last {
@@ -53,6 +93,15 @@ impl Bracket {
             Bracket::Square => 57,
             Bracket::Curly => 1197,
             Bracket::Angle => 25137
+        }
+    }
+
+    fn point2(&self) -> u64 {
+        match self {
+            Bracket::Parentheses => 1,
+            Bracket::Square => 2,
+            Bracket::Curly => 3,
+            Bracket::Angle => 4,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ aoc_main::main! {
     day7 : generator => part1, part2;
     day8 : generator => part1, part2;
     day9 : generator => part1, part2;
-    day10: generator => part1;
+    day10: generator => part1, part2;
 }
 
 mod day1;
